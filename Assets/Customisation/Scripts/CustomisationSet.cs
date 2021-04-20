896514b73a16faede7245620d17540921432b859
Kieran <73572378+KieranBDW@users.noreply.github.com>
2021-04-20 23:09:49 +1000

    1 done

--- Assets/Customisation/Scripts/CustomisationSet.cs
+++ Assets/Customisation/Scripts/CustomisationSet.cs
@@ -31,9 +31,13 @@ public class CustomisationSet : MonoBehaviour
     [Header("Max amount of textures per type")]
     public int skinMax;
     public int eyesMax, mouthMax, hairMax, armourMax, clothesMax;
+    [Header("Mat Name")]
+    public string[] matName = new string[6];
 
     private void Start()
     {
+        matName = new string[] { "Skin", "Eyes", "Mouth", "Hair", "Clothes", "Armour" };
+
         selectedClass = new string[] { "Barbarian", "Bard", "Druid", "Monk", "Paladin", "Ranger", "Sorcerer", "Warlock" };
 
         for (int i = 0; i < skinMax; i++)
@@ -41,7 +45,6 @@ public class CustomisationSet : MonoBehaviour
             Texture2D tempTexture = Resources.Load("Character/Skin_" + i) as Texture2D;
             skin.Add(tempTexture);
         }
-
         for (int i = 0; i < eyesMax; i++)
         {
             Texture2D tempTexture = Resources.Load("Character/Eyes_" + i) as Texture2D;
@@ -66,6 +69,122 @@ public class CustomisationSet : MonoBehaviour
         {
             Texture2D tempTexture = Resources.Load("Character/Armour_" + i) as Texture2D;
             armour.Add(tempTexture);
+        }
+    }
+    
+    void SetTexture(string type, int dir)
+    {
+        int index = 0, max = 0, matIndex = 0;
+        Texture2D[] textures = new Texture2D[0];
+
+        switch (type)
+        {
+            case "Skin":
+                index = skinIndex;
+                max = skinMax;
+                textures = skin.ToArray();
+                matIndex = 1;
+                break;
+            case "Eyes":
+                index = eyesIndex;
+                max = eyesMax;
+                textures = eyes.ToArray();
+                matIndex = 2;
+                break;
+            case "Mouth":
+                index = mouthIndex;
+                index = mouthIndex;
+                max = mouthMax;
+                textures = mouth.ToArray();
+                matIndex = 3;
+                break;
+            case "Hair":
+                index = hairIndex;
+                index = hairIndex;
+                max = hairMax;
+                textures = hair.ToArray();
+                matIndex = 4;
+                break;
+            case "Clothes":
+                index = clothesIndex;
+                index = clothesIndex;
+                max = clothesMax;
+                textures = clothes.ToArray();
+                matIndex = 5;
+                break;
+            case "Armour":
+                index = armourIndex;
+                index = armourIndex;
+                max = armourMax;
+                textures = armour.ToArray();
+                matIndex = 6;
+                break;
+
+        }
+        index += dir;
+        if(index < 0)
+        {
+            index = max - 1;
+        }
+        if(index > max - 1)
+        {
+            index = 0;
+        }
+        Material[] mat = characterRenderer.materials;
+        mat[matIndex].mainTexture = textures[index];
+        characterRenderer.materials = mat;
+
+        switch (type)
+        {
+            case "Skin":
+                skinIndex = index;
+                break;
+            case "Eyes":
+                eyesIndex = index;
+                break;
+            case "Mouth":
+                mouthIndex = index;
+                break;
+            case "Hair":
+                hairIndex = index;
+                break;
+            case "Clothes":
+                clothesIndex = index;
+                break;
+            case "Armour":
+                armourIndex = index;
+                break;
+        }
+    }
+
+    private void OnGUI()
+    {
+        // I'm going to try make the following neater and easier to follow.
+        #region GUI value setup
+        // 16:9
+        Vector2 scr = new Vector2(Screen.width / 16, Screen.height / 9);
+        // Shuffle GUI down the screen.
+
+        // Start positions.
+        float left = 0.25f * scr.x;
+        float mid = 0.75f * scr.x;
+        float right = 2.25f * scr.x;
+        // Sizes.
+        float x = 0.5f * scr.x;
+        float y = 0.5f * scr.x;
+        float lable = 1.5f * scr.x;
+        #endregion
+        for (int i = 0; i < matName.Length; i++)
+        {
+            if (GUI.Button(new Rect(left, y + i * y, x, y), "<"))
+            {
+                SetTexture(matName[i], -1);
+            }
+            GUI.Box(new Rect(mid, y + i * y, lable, y), matName[i]);
+            if (GUI.Button(new Rect(right, y + i * y, x, y), ">"))
+            {
+                SetTexture(matName[i], 1);
+            }
         }
     }
 }
