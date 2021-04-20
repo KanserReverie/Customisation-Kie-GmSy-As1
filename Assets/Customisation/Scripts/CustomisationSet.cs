157c79eaed804b4b5cfdca349b7e555a7d730c35
Kieran <73572378+KieranBDW@users.noreply.github.com>
2021-04-21 01:10:14 +1000

    dd

--- Assets/Customisation/Scripts/CustomisationSet.cs
+++ Assets/Customisation/Scripts/CustomisationSet.cs
@@ -1,6 +1,8 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using System.IO;
+using System.Runtime.Serialization.Formatters.Binary;
 using UnityEngine.SceneManagement;
 
 public class CustomisationSet : MonoBehaviour
@@ -8,9 +10,17 @@ public class CustomisationSet : MonoBehaviour
     [Header("Character Name")]
     public string characterName;
     [Header("Character Class")]
-    public CharacterClass characerClass = CharacterClass.Barbarian;
+    public CharacterClass characterClass = CharacterClass.Barbarian;
     public string[] selectedClass = new string[8];
     public int selectedClassIndex = 0;
+    [System.Serializable]
+    public struct Stats
+    {
+        public string baseStatsName;
+        public int baseStats;
+        public int tempStats;
+    };
+    public Stats[] characterStats;
     [Header("Dropdown Menu")]
     public bool showDropdown;
     public Vector2 scrollPos;
@@ -157,6 +167,132 @@ public class CustomisationSet : MonoBehaviour
         }
     }
 
+    void ChooseClass(int classIndex)
+    {
+        // Will come back to this.
+        switch (classIndex)
+        {
+            case 0:
+                characterStats[0].baseStats = 18;
+                characterStats[1].baseStats = 10;
+                characterStats[2].baseStats = 15;
+                characterStats[3].baseStats = 6;
+                characterStats[4].baseStats = 6;
+                characterStats[5].baseStats = 5;
+
+                characterClass = CharacterClass.Barbarian;
+                break;
+            case 1:
+                characterStats[0].baseStats = 6;
+                characterStats[1].baseStats = 13;
+                characterStats[2].baseStats = 7;
+                characterStats[3].baseStats = 10;
+                characterStats[4].baseStats = 6;
+                characterStats[5].baseStats = 18;
+
+                characterClass = CharacterClass.Bard;
+                break;
+            case 2:
+                characterStats[0].baseStats = 5;
+                characterStats[1].baseStats = 8;
+                characterStats[2].baseStats = 8;
+                characterStats[3].baseStats = 9;
+                characterStats[4].baseStats = 15;
+                characterStats[5].baseStats = 15;
+
+                characterClass = CharacterClass.Druid;
+                break;
+            case 3:
+                characterStats[0].baseStats = 8;
+                characterStats[1].baseStats = 15;
+                characterStats[2].baseStats = 10;
+                characterStats[3].baseStats = 15;
+                characterStats[4].baseStats = 8;
+                characterStats[5].baseStats = 4;
+
+                characterClass = CharacterClass.Monk;
+                break;
+            case 4:
+                characterStats[0].baseStats = 15;
+                characterStats[1].baseStats = 6;
+                characterStats[2].baseStats = 10;
+                characterStats[3].baseStats = 8;
+                characterStats[4].baseStats = 5;
+                characterStats[5].baseStats = 18;
+
+                characterClass = CharacterClass.Paladin;
+                break;
+            case 5:
+
+                characterStats[0].baseStats = 8;
+                characterStats[1].baseStats = 16;
+                characterStats[2].baseStats = 8;
+                characterStats[3].baseStats = 12;
+                characterStats[4].baseStats = 8;
+                characterStats[5].baseStats = 8;
+                
+                characterClass = CharacterClass.Ranger;
+                break;
+            case 6:
+
+                characterStats[0].baseStats = 6;
+                characterStats[1].baseStats = 8;
+                characterStats[2].baseStats = 16;
+                characterStats[3].baseStats = 8;
+                characterStats[4].baseStats = 8;
+                characterStats[5].baseStats = 14;
+                
+                characterClass = CharacterClass.Sorcerer;
+                break;
+            case 7:
+
+                characterStats[0].baseStats = 6;
+                characterStats[1].baseStats = 6;
+                characterStats[2].baseStats = 6;
+                characterStats[3].baseStats = 10;
+                characterStats[4].baseStats = 14;
+                characterStats[5].baseStats = 18;
+                
+                characterClass = CharacterClass.Warlock;
+                break;
+        }
+    }
+
+    public void SaveCharacter()
+    {
+        // Binary stuff, will get to it later.
+        //////// Converting our game classes into something we can write to a file.
+        //////PlayerData data = new PlayerData();
+
+        //////// Creating a new binary formatter.
+        //////BinaryFormatter formatter = new BinaryFormatter();
+        //////// Location we are saving the file.
+        //////string path = Application.dataPath + "/BSave.this";
+        //////// Creating a file at the file path
+        //////FileStream stream = new FileStream(path, FileMode.Create);
+
+        //////// Conver a class into a string that can be written to the file
+        //////formatter.Serialize(stream, data);
+
+        //////stream.Close();
+
+        PlayerPrefs.SetInt("SkinIndex", skinIndex);
+        PlayerPrefs.SetInt("HairIndex", hairIndex);
+        PlayerPrefs.SetInt("EyesIndex", eyesIndex);
+        PlayerPrefs.SetInt("MouthIndex", mouthIndex);
+        PlayerPrefs.SetInt("ClothesIndex", clothesIndex);
+        PlayerPrefs.SetInt("ArmourIndex", armourIndex);
+
+        PlayerPrefs.SetString("characterName", characterName);
+
+        for (int i = 0; i < characterStats.Length; i++)
+        {
+            PlayerPrefs.SetInt(characterStats[i].baseStatsName, characterStats[i].baseStats + characterStats[i].tempStats);
+        }
+        PlayerPrefs.SetString("CharacterClass", selectedClass[selectedClassIndex]);
+    }
+
+
     private void OnGUI()
     {
         // I'm going to try make the following neater and easier to follow.
@@ -174,6 +310,7 @@ public class CustomisationSet : MonoBehaviour
         float y = 0.5f * scr.x;
         float lable = 1.5f * scr.x;
         #endregion
+        #region Customisation Textures
         for (int i = 0; i < matName.Length; i++)
         {
             if (GUI.Button(new Rect(left, y + i * y, x, y), "<"))
@@ -186,6 +323,35 @@ public class CustomisationSet : MonoBehaviour
                 SetTexture(matName[i], 1);
             }
         }
+        #endregion
+        #region Choose Class
+        float classX = 12.75f * scr.x;
+        float h = 0;
+        if(GUI.Button(new Rect(classX,y+h*y,4*x,y),classButton))
+        {
+            showDropdown = !showDropdown;
+        }
+        h++;
+        if(showDropdown)
+        {
+            scrollPos = GUI.BeginScrollView(
+                new Rect(classX, y + h * y, 4 * x, 4 * y), scrollPos,
+                new Rect(0, 0, 0, selectedClass.Length * y), false, true);
+
+            for (int i = 0; i < selectedClass.Length; i++)
+            {
+                if(GUI.Button(new Rect(0, i*y, 3*x, y), selectedClass[i]))
+                {
+                    ChooseClass(i);
+                    classButton = selectedClass[i];
+                    showDropdown = false;
+                }
+            } 
+        }
+        #endregion
+        #region Set Stats
+
+        #endregion
     }
 }
 
